d375f33d6ea8295f5b59a0f12319437b038b8a30
aozp73 <aozp73@naver.com>
2023-02-09 12:12:41 +0900

    프로젝트 단계별 문서정리 완료(README생성)

--- src/main/java/shop/mtcoding/blog2/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog2/controller/UserController.java
@@ -1,13 +1,8 @@
 package shop.mtcoding.blog2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/tes")
-    public String tes() {
-        return "/board/tes";
-    }
 }
