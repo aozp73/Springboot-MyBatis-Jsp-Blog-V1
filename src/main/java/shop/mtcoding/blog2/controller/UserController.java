7c4e958d3f47951676c5cbc9f2550acd42563d23
aozp73 <aozp73@naver.com>
2023-02-09 12:51:24 +0900

    CustomException 생성완료

--- src/main/java/shop/mtcoding/blog2/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog2/controller/UserController.java
@@ -1,13 +1,8 @@
 package shop.mtcoding.blog2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String main() {
-        return "board/main";
-    }
 }
---
+++ src/main/java/shop/mtcoding/blog2/handler/ex/CustomApiException.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.blog2.handler.ex;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class CustomApiException extends RuntimeException {
+
+    private HttpStatus status;
+
+    public CustomApiException(String msg, HttpStatus status) {
+        super(msg);
+        this.status = status;
+    }
+
+    public CustomApiException(String msg) {
+        this(msg, HttpStatus.BAD_REQUEST);
+    }
+
+}
